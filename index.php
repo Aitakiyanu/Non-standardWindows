f17728bc91130d62907842f45e0f31bcf0740383
Artyom Vozhegov <agamemnon5@yandex.ru>
2020-10-22 15:39:02 +0300

    Добавлена визуализация прямоугольного проема с окном

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
     <fieldset>
         <legend>Размеры проема</legend>
         <label for="wall_opening_width">Ширина:</label>
-        <input id="wall_opening_width" type="number" name="wall_opening_width"><br/>
+        <input id="wall_opening_width" type="number" name="wall_opening_width" value="1300"><br/>
         <label for="wall_opening_height">Высота:</label>
-        <input id="wall_opening_height" type="number" name="wall_opening_height"><br/>
+        <input id="wall_opening_height" type="number" name="wall_opening_height" value="1400"><br/>
         <label for="assembly_seam">Монтажный шов (минус, если заход за четверть)</label>
         <input id="assembly_seam" type="number" name="assembly_seam" value="20"><br/>
         <label for="bottom_assembly_seam">Монтажный шов снизу, включая подставочный (минус, если заход окна за четверть)</label>
--- rectwincalc.php
+++ rectwincalc.php
@@ -8,12 +8,26 @@
 <?php
 $hsize = $_GET["wall_opening_width"] - $_GET["assembly_seam"] * 2;
 $vsize = $_GET["wall_opening_height"] - $_GET["assembly_seam"] - $_GET["bottom_assembly_seam"];
+$rect_size = max($_GET["wall_opening_width"], $_GET["wall_opening_height"]);
+if ($_GET["assembly_seam"] < 0 || $_GET["bottom_assembly_seam"] < 0) {
+    $rect_size = $rect_size + max(abs($_GET["assembly_seam"]), abs($_GET["bottom_assembly_seam"]));
+}
 ?>
-<h3><u>Размеры проема</u></h3>
+<h3 style="color: red"><u>Размеры проема</u></h3>
 <p>Ширина проема: <?php echo $_GET["wall_opening_width"]; ?> мм</p>
 <p>Высота проема: <?php echo $_GET["wall_opening_height"]; ?> мм</p>
-<h3><u>Размеры окна</u></h3>
+<h3 style="color: blue"><u>Размеры окна</u></h3>
 <p>Ширина окна: <?php echo $hsize; ?> мм</p>
 <p>Высота окна: <?php echo $vsize; ?> мм</p>
+<h3><u>Визуализация</u></h3>
+<svg width="200" height="200"
+     viewBox="0 0 <?php echo $rect_size + max(abs($_GET["assembly_seam"]), abs($_GET["bottom_assembly_seam"]));?> <?php echo $rect_size + 20;?>"
+     xmlns="http://www.w3.org/2000/svg">
+    <!-- Рисуем проем -->
+    <rect x="<?php echo max(10,-$_GET["assembly_seam"]+2);?>" y="<?php echo max(10,-$_GET["assembly_seam"]+2);?>" width="<?php echo $_GET["wall_opening_width"];?>" height="<?php echo $_GET["wall_opening_height"];?>" stroke="red" stroke-width="0.1%" fill="none"></rect>
+    <!-- Рисуем окно -->
+    <rect x="<?php echo $_GET["assembly_seam"] + max(10,-$_GET["assembly_seam"]+2);?>" y="<?php echo $_GET["assembly_seam"] + max(10,-$_GET["assembly_seam"]+2);?>" width="<?php echo $hsize;?>" height="<?php echo $vsize;?>" stroke="blue" stroke-width="0.1%" fill="none"></rect>
+
+</svg>
 </body>
 </html>
