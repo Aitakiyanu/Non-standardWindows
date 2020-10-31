9300db6659edf7f390245d74b253346fa4482480
Artyom Vozhegov <agamemnon5@yandex.ru>
2020-10-31 15:37:44 +0300

    Начал заводить переменные. Пока не работает

--- octawincalc.php
+++ octawincalc.php
@@ -2,9 +2,29 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>$Title$</title>
+    <title>Размеры окна</title>
 </head>
 <body>
-$END$
+<?php
+/*$wob = $_GET["wall_opening_bottom"];
+$wobl = $_GET["wall_opening_bottom_to_left"];
+$woblw = $_GET["wall_opening_bottom_to_left_width"];
+$woblh = $_GET["wall_opening_bottom_to_left_height"];
+$wol = $_GET["wall_opening_left"];
+$wolt = $_GET["wall_opening_left_to_top"];
+$woltw = $_GET["wall_opening_left_to_top_width"];
+$wolth = $_GET["wall_opening_left_to_top_height"];
+$wot = $_GET["wall_opening_top"];
+$wotr = $_GET["wall_opening_top_to_right"];
+$wotrw = $_GET["wall_opening_top_to_right_width"];
+$wotrh = $_GET["wall_opening_top_to_right_height"];
+$wor = $_GET["wall_opening_right"];
+$worb = $_GET["wall_opening_right_to_bottom"];
+$worbw = $_GET["wall_opening_right_to_bottom_width"];
+$worbh = $_GET["wall_opening_right_to_bottom_height"];
+echo $_GET["negate_left_assembly_seam"];*/
+print_r($_GET);
+?>
+
 </body>
 </html>
