f8620d936d857d90b6b8797192870d85a40645fe
Artyom Vozhegov <agamemnon5@yandex.ru>
2020-11-06 19:29:05 +0300

    Что-то не то с коэффициентами (много нулей)

--- index.php
+++ index.php
@@ -15,10 +15,15 @@ include_once 'src/form.php';
 if (count($_POST) > 0) {
     include_once 'src/WallOpeningSide.php';
     include_once 'src/Wall-Opening-Sides-Array.php';
-};
+}
+
 if (isset($wallOpeningSides)) {
-    foreach ($wallOpeningSides as $key => $value)
-};
+    echo " A    B    C<br/>";
+    foreach ($wallOpeningSides as $key => $value) {
+        echo $key.': '.$value->newCoefficientA.'->'.$value->coefficientA.' # '.$value->newCoefficientB.'->'.$value->coefficientB.' # '.$value->newCoefficientC.'->'.$value->coefficientC.'<br/>';
+    }
+}
+
 ?>
 </body>
 </html>
--- src/WallOpeningSide.php
+++ src/WallOpeningSide.php
@@ -13,6 +13,13 @@ class WallOpeningSide
     public int $coefficientA; //коэффициент A в функции прямой Ax+By+C=0
     public int $coefficientB; //коэффициент B в функции прямой Ax+By+C=0
     public int $coefficientC; //коэффициент C в функции прямой Ax+By+C=0
+    private $auxiliaryStartX;
+    private $auxiliaryStartY;
+    private $auxiliaryEndX;
+    private $auxiliaryEndY;
+    public $newCoefficientA;
+    public $newCoefficientB;
+    public $newCoefficientC;
     public function __construct($sideLength, $sideWidth, $sideHeight, $sideSeam, $previousEndX, $previousEndY)
     {
         $this->sideLength = $sideLength;
@@ -37,29 +44,42 @@ class WallOpeningSide
             $widthDifference = abs($newSideWidth - $sideWidth);
             if ($this->sideWidth > 0) {
                 if ($this->sideHeight >0) {
-                    $auxiliaryStartX = $this->startX;
-                    $auxiliaryStartY = $this->startY - $heightDifference;
-                    $auxiliaryEndX = $this->endX + $widthDifference;
-                    $auxiliaryEndY = $this->endY;
+                    $this->auxiliaryStartX = $this->startX;
+                    $this->auxiliaryStartY = $this->startY - $heightDifference;
+                    $this->auxiliaryEndX = $this->endX + $widthDifference;
+                    $this->auxiliaryEndY = $this->endY;
                 } else {
-                    $auxiliaryStartX = $this->startX - $widthDifference;
-                    $auxiliaryStartY = $this->startY;
-                    $auxiliaryEndX = $this->endX;
-                    $auxiliaryEndY = $this->endY - $heightDifference;
+                    $this->auxiliaryStartX = $this->startX - $widthDifference;
+                    $this->auxiliaryStartY = $this->startY;
+                    $this->auxiliaryEndX = $this->endX;
+                    $this->auxiliaryEndY = $this->endY - $heightDifference;
                 }
             } else {
                 if ($this->sideHeight >0) {
-                    $auxiliaryStartX = $this->startX + $widthDifference;
-                    $auxiliaryStartY = $this->startY;
-                    $auxiliaryEndX = $this->endX;
-                    $auxiliaryEndY = $this->endY + $heightDifference;
+                    $this->auxiliaryStartX = $this->startX + $widthDifference;
+                    $this->auxiliaryStartY = $this->startY;
+                    $this->auxiliaryEndX = $this->endX;
+                    $this->auxiliaryEndY = $this->endY + $heightDifference;
                 } else {
-                    $auxiliaryStartX = $this->startX;
-                    $auxiliaryStartY = $this->startY + $heightDifference;
-                    $auxiliaryEndX = $this->endX + $widthDifference;
-                    $auxiliaryEndY = $this->endY;
+                    $this->auxiliaryStartX = $this->startX;
+                    $this->auxiliaryStartY = $this->startY + $heightDifference;
+                    $this->auxiliaryEndX = $this->endX + $widthDifference;
+                    $this->auxiliaryEndY = $this->endY;
                 }
             }
+        } elseif ($this->sideWidth = 0 && $this->sideHeight <> 0) {
+            $this->auxiliaryStartY = $this->startY;
+            $this->auxiliaryEndY = $this->endY;
+            $this->auxiliaryStartX = $this->startX + ($this->sideHeight <=> 0) * $this->sideSeam;
+            $this->auxiliaryEndX = $this->endX + ($this->sideHeight <=> 0) * $this->sideSeam;
+        } elseif ($this->sideHeight = 0 && $this->sideWidth <> 0) {
+            $this->auxiliaryStartX = $this->startX;
+            $this->auxiliaryEndX = $this->endX;
+            $this->auxiliaryStartY = $this->startY - ($this->sideWidth <=> 0) * $this->sideSeam;
+            $this->auxiliaryEndY = $this->endY - ($this->sideWidth <=> 0) * $this->sideSeam;
         }
+        $this->newCoefficientA = $this->auxiliaryStartY - $this->auxiliaryEndY;
+        $this->newCoefficientB = $this->auxiliaryEndX - $this->auxiliaryStartX;
+        $this->newCoefficientC = $this->auxiliaryStartX * $this->auxiliaryEndY - $this->auxiliaryEndX * $this->auxiliaryStartY;
     }
 }
