653fad394bcfe57e13b4f4c63dd5fad523b5dadb
Artyom Vozhegov <agamemnon5@yandex.ru>
2020-11-07 15:02:45 +0300

    В процессе

--- src/intersections-calculation.php
+++ src/intersections-calculation.php
@@ -1,6 +1,21 @@
 <?php
 $intersections = [];
-for ($i = 1, $size = count($wallOpeningSides); $i <= $size; $i++) {
-    if ($i == 3) continue;
-    echo $i.'<br/>';
+$strLines = [];
+$i = 0;
+
+foreach ($wallOpeningSides as $key => $item) {
+    if ($item->newCoefficientC <> 0) {
+        $strLines[$i] = $key;
+        $i++;
+    }
+}
+
+if (count($strLines)>2) {
+    for ($i = 0, $size = count($strLines); $i < $size; $i++) {
+        $firstStrLine = $strLines[$i];
+        $secondStrLine = ($i == $size - 1) ? $strLines[0] : $strLines[$i + 1];
+
+    }
+} else {
+    echo 'Сторон должно быть больше двух';
 }
