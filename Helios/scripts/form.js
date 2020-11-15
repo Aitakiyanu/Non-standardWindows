485481c1607f751e485019fb55dd122534c62603
Artyom Vozhegov <agamemnon5@yandex.ru>
2020-11-15 18:35:16 +0300

    В процессе

--- Helios/scripts/form.js
+++ Helios/scripts/form.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let sideForms = document.getElementsByClassName('side_form');
+let sideForms = document.querySelectorAll('.side_form');
 let sidesCount = sideForms.length;
 if (sidesCount === 0) {
     let addButton = document.createElement('input');
@@ -14,24 +14,45 @@ if (sidesCount === 0) {
 
 
 let entForm = document.getElementById('entire_form');
+//Вешаем eventListener на всю форму
 entForm.addEventListener('click', function (event) {
+    //Если клик по названию стороны - скрываем-показываем поля ввода, показываем-скрываем длину стороны
     if (event.target.dataset.type === 'legend') {
         let itemsToHide = event.target.parentNode.querySelectorAll('*:not([type="button"])');
         let itemsCount = itemsToHide.length;
-        console.log(itemsToHide);
         for (let i = 0; i < itemsCount; i++) {
             if (!itemsToHide[i].classList.contains('side_form_legend')) {
                 itemsToHide[i].hidden = !itemsToHide[i].hidden;
             }
         }
-    } else if (event.target.dataset.type === 'addside') {
-        let thisSideNumber = event.target.parentNode.id.slice(event.target.parentNode.id.lastIndexOf('_') + 1);
-        let formtags = event.target.parentNode.querySelectorAll('*');
-        let formtagsCount = formtags.length;
-        let numberToAdd = thisSideNumber + 1;
-        for (let i = numberToAdd; i < formtagsCount; i++) {
-
+    } else if (event.target.dataset.type === 'addside') { //Добавить сторону
+        let newSide = event.target.parentNode.cloneNode(true);
+        //Получаем номер стороны,в которой произошел клик, в коллекции сторон (поэтому -1)
+        let thisSideCollectionIndex = event.target.parentNode.id.slice(event.target.parentNode.id.lastIndexOf('_') + 1) - 1;
+        for (let side = thisSideCollectionIndex; side < sidesCount; side++) {
+            //Следующий номер для атрибутов элемента равен текущему номеру в аттрибуте +1, то есть номеру индекса +2
+            let nextSide = side + 2;
+            if (sideForms[side].hasAttribute('id')) {
+                sideForms[side].id = sideForms[side].id.slice(0, sideForms[side].id.lastIndexOf('_') + 1) + nextSide;
+            }
+            //Получаем все элементы внутри родительского кнопки, в которой произошел клик (добавить проверку нулевого количества сторон)
+            let formtags = sideForms[side].querySelectorAll('*');
+            //Вспомогательная переменная с количеством элементов для цикла
+            let formtagsCount = formtags.length;
+            //Изменяем номер в атрибутах всех сторон после текущей, если она не последняя
+            for (let i = 0; i < formtagsCount; i++) {
+                if (formtags[i].hasAttribute('id')) {
+                    formtags[i].id = formtags[i].id.slice(0, formtags[i].id.lastIndexOf('_') + 1) + nextSide;
+                }
+                if (formtags[i].hasAttribute('name')) {
+                    formtags[i].setAttribute('name', formtags[i].getAttribute('name').slice(0, formtags[i].getAttribute('name').lastIndexOf('_') + 1) + nextSide);
+                }
+                if (formtags[i].hasAttribute('for')) {
+                    formtags[i].setAttribute('for', formtags[i].getAttribute('for').slice(0, formtags[i].getAttribute('for').lastIndexOf('_') + 1) + nextSide);
+                }
+            }
         }
+        event.target.parentElement.before(newSide);
     } else if (event.target.dataset.type === 'removeside') {
         event.target.parentElement.remove();
         if (event.target.parentElement.nextElementSibling.classList.contains('side_form')) {
