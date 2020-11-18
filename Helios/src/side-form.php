248fc583a88519d47f37bd1c1498429b04661b59
Artyom Vozhegov <agamemnon5@yandex.ru>
2020-11-18 11:47:34 +0300

    В процессе

--- Helios/src/side-form.php
+++ Helios/src/side-form.php
@@ -1,10 +1,14 @@
 <?php //Начнем слева
 namespace Helios;
 
-    $sidesCount = 8;
-    $wallOpeningSides = [];
 
 include_once 'wall-opening-sides-array.php';
+
+if (isset($sidesCount)) {
+    echo $sidesCount;
+} else {
+    $sidesCount = 8;
+}
 
 for ($i = 1; $i <= $sidesCount; $i++) {
     $negateChecked = $_POST["negate_assembly_seam_{$i}"] ?? "";
@@ -25,16 +29,16 @@ for ($i = 1; $i <= $sidesCount; $i++) {
     echo <<<SIDEFORM
 <fieldset id="side_form_{$i}" class="side_form">
     <legend class="side_form_legend" data-type="legend">Сторона $i</legend>
-    <span id="side_{$i}" hidden>{$wallOpeningSides[$i]->sideLength}</span>
+    <span id="side_{$i}" hidden>{$_POST["wall_opening_side_length_{$i}"]}</span>
     
     <label for="wall_opening_side_length_{$i}">Длина стороны:</label>
-    <input id="wall_opening_side_length_{$i}" type="number" name="wall_opening_side_length_{$i}" value="{$wallOpeningSides[$i]->sideLength}" min="0" required>
+    <input id="wall_opening_side_length_{$i}" type="number" name="wall_opening_side_length_{$i}" value="{$_POST["wall_opening_side_length_{$i}"]}" min="0" required>
      
     <input type="button" id="add_side_{$i}" value="Добавить после" data-type="addside"/>
     <input type="button" id="remove_side_{$i}" value="Убрать сторону" data-type="removeside"/><br/>
    
     <label for="wall_opening_side_width_{$i}">Ширина по горизонтали:</label>
-    <input type="number" id="wall_opening_side_width_{$i}" name="wall_opening_side_width_{$i}" value="{$wallOpeningSides[$i]->sideWidth}" required>
+    <input type="number" id="wall_opening_side_width_{$i}" name="wall_opening_side_width_{$i}" value="{$_POST["wall_opening_side_width_{$i}"]}" required>
     
     <label for="right_direction_{$i}">вправо(+)</label>
     <input id="right_direction_{$i}" type="radio" name="left_or_right_{$i}" value="1" required {$rightChecked}>
@@ -43,7 +47,7 @@ for ($i = 1; $i <= $sidesCount; $i++) {
     <input id="left_direction_{$i}" type="radio" name="left_or_right_{$i}" value="-1" required {$leftChecked}><br/>
     
     <label for="wall_opening_side_height_{$i}">Высота по вертикали:</label>
-    <input type="number" id="wall_opening_side_height_{$i}" name="wall_opening_side_height_{$i}" value="{$wallOpeningSides[$i]->sideHeight}" required>
+    <input type="number" id="wall_opening_side_height_{$i}" name="wall_opening_side_height_{$i}" value="{$_POST["wall_opening_side_height_{$i}"]}" required>
     
     <label for="up_direction_{$i}">вверх(+)</label>
     <input id="up_direction_{$i}" type="radio" name="up_or_down_{$i}" value="1" required {$upChecked}>
@@ -52,7 +56,7 @@ for ($i = 1; $i <= $sidesCount; $i++) {
     <input id="down_direction_{$i}" type="radio" name="up_or_down_{$i}" value="-1" required {$downChecked}><br/>
     
     <label for="side_assembly_seam_{$i}">Шов:</label>
-    <input id="side_assembly_seam_{$i}" type="number" name="side_assembly_seam_{$i}" value="{$wallOpeningSides[$i]->sideSeam}" min="0" required>
+    <input id="side_assembly_seam_{$i}" type="number" name="side_assembly_seam_{$i}" value="{$_POST["side_assembly_seam_{$i}"]}" min="0" required>
     
     <label for="negate_assembly_seam_{$i}">Или это заход за четверть?</label>
     <input id="negate_assembly_seam_{$i}" type="checkbox" name="negate_assembly_seam_{$i}" value="checked" {$negateChecked}>
--- Helios/src/wall-opening-sides-array.php
+++ Helios/src/wall-opening-sides-array.php
@@ -3,28 +3,23 @@ namespace Helios;
 
 include_once 'WallOpeningSide.php';
 
+$wallOpeningSides = [];
+$sidesCount = 0;
+
 if (count($_POST) > 0) {
-    if (isset($sidesCount)) {
+    while (isset($_POST["remove_side_{$sidesCount}"])) {
+        $sidesCount++;
+    }
         for ($i = 1; $i <= $sidesCount; $i++) {
             $seamSign = isset($_POST["negate_assembly_seam_{$i}"]) ? -1 : 1;
             $prvSide = ($i === 1 ? $sidesCount : $i) - 1;
             $prvSideX = isset($wallOpeningSides[$prvSide]) ? $wallOpeningSides[$prvSide]->endX : 0;
             $prvSideY = isset($wallOpeningSides[$prvSide]) ? $wallOpeningSides[$prvSide]->endY : 0;
             $wallOpeningSides[$i] = new WallOpeningSide($_POST["wall_opening_side_length_{$i}"],
-                $_POST["wall_opening_side_width_{$i}"],
-                $_POST["wall_opening_side_height_{$i}"],
+                $_POST["wall_opening_side_width_{$i}"] * ($_POST["left_or_right_{$i}"] <=> 0),
+                $_POST["wall_opening_side_height_{$i}"] * ($_POST["up_or_down_{$i}"] <=> 0),
                 $seamSign * $_POST["side_assembly_seam_{$i}"],
                 $prvSideX,
                 $prvSideY);
         }
     }
-} else {
-    $wallOpeningSides[1] = new WallOpeningSide(500, -354, 354, 20, 354, 0);
-    $wallOpeningSides[2] = new WallOpeningSide(500, 0, 500, 20, $wallOpeningSides[1]->endX, $wallOpeningSides[1]->endY);
-    $wallOpeningSides[3] = new WallOpeningSide(500, 354, 354, 20, $wallOpeningSides[2]->endX, $wallOpeningSides[2]->endY);
-    $wallOpeningSides[4] = new WallOpeningSide(500, 500, 0, 20, $wallOpeningSides[3]->endX, $wallOpeningSides[3]->endY);
-    $wallOpeningSides[5] = new WallOpeningSide(500, 354, -354, 20, $wallOpeningSides[4]->endX, $wallOpeningSides[4]->endY);
-    $wallOpeningSides[6] = new WallOpeningSide(500, 0, -500, 20, $wallOpeningSides[5]->endX, $wallOpeningSides[5]->endY);
-    $wallOpeningSides[7] = new WallOpeningSide(500, -354, -354, 20, $wallOpeningSides[6]->endX, $wallOpeningSides[6]->endY);
-    $wallOpeningSides[8] = new WallOpeningSide(500, -500, 0, 50, $wallOpeningSides[7]->endX, $wallOpeningSides[7]->endY);
-}
