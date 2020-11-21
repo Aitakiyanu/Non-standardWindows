bc247ce8c81475faaa6f568eb4ea447d615703c5
Artyom Vozhegov <agamemnon5@yandex.ru>
2020-11-21 17:43:48 +0300

    SVG проба

--- Helios/scripts/visualization.js
+++ Helios/scripts/visualization.js
@@ -1,17 +1,21 @@
-let outline = document.createElement('div');
-outline.id = 'outline';
-let svgOutline = document.createElement('svg');
-svgOutline.setAttribute('width', '200');
-svgOutline.setAttribute('height', '200');
-svgOutline.setAttribute('viewBox', '0 0 200 200');
-svgOutline.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-let outlineFigure = document.createElement('polygon');
-outlineFigure.setAttribute('points', '10, 10 190,10 190,190 10,190');
-outlineFigure.setAttribute('fill', 'none');
-outlineFigure.setAttribute('stroke', 'green');
-document.body.append('outline');
-console.log(outline === document.getElementById('outline'));
-//outline = document.getElementById('outline');
-//outline.append('svgOutline');
-//svgOutline = outline.getElementsByTagName('svg')[0];
-//svgOutline.append('outlineFigure');
+let newdiv = document.createElement('div');
+newdiv.id = "newdiv";
+newdiv.innerHTML = `<svg width="200" height="200" viewBox="0 0 200 200"><polygon points="10,10 190,10 190,190 10,190" fill="none" stroke="green"></polygon></svg>`;
+document.body.firstElementChild.after(newdiv);
+let a = 10;
+let b = 10;
+let c = 100;
+let d = 190;
+let e = 190;
+let f = 10
+newdiv.getElementsByTagName('polygon')[0].setAttribute('points', `${a},${b} ${c},${d} ${e},${f}`);
+/*let svgOutline = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+svgOutline.setAttributeNS(null, 'width', '200');
+svgOutline.setAttributeNS(null, 'height', '200');
+svgOutline.setAttributeNS(null, 'viewBox', '0 0 200 200');
+let outlineFigure = document.createElementNS("http://www.w3.org/2000/svg", 'polygon');
+outlineFigure.setAttributeNS(null, 'points', '10, 10 190,10 190,190 10,190');
+outlineFigure.setAttributeNS(null, 'fill', 'none');
+outlineFigure.setAttributeNS(null, 'stroke', 'green');
+newdiv.append(svgOutline);
+svgOutline.append(outlineFigure);*/
